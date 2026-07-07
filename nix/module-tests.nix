9262e94e64519dae0769c052534e9a4d2b5aa62d
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-07 18:51:25 +0200

    Simplify SmartCool NixOS module

--- nix/module-tests.nix
+++ nix/module-tests.nix
@@ -25,79 +25,23 @@
       ];
     };
 
-  firmwareSystem = eval {
-    services.smartcool.firmwareAttributes = {
-      enable = true;
-      basePath = "/sys/class/firmware-attributes/asus-armoury/attributes";
-      attributes = {
-        nv_temp_target = 75;
-        ppt_pl1_spl = 15;
-      };
-      reassertInterval = "60s";
+  basicSystem = eval {
+    services.smartcool.settings = {
+      poll_interval_ms = 2000;
+      sensors = [];
+      fans = [];
     };
   };
 
-  nvidiaSystem = eval {
-    services.smartcool.nvidiaClockCap = {
-      enable = true;
-      nvidiaSmi = "/run/current-system/sw/bin/nvidia-smi";
-      minClockMHz = 210;
-      maxClockMHz = 600;
-      reassertInterval = "30s";
-    };
-  };
-
-  fanCurveSystem = eval {
-    services.smartcool.asusdFanCurves = {
-      enable = true;
-      profiles.quiet = [
-        {
-          fan = "CPU";
-          pwm = [0 0 0 0 30 80 140 200];
-          temp = [40 55 65 73 78 84 90 95];
-        }
-        {
-          fan = "GPU";
-          pwm = [0 0 0 0 30 80 140 200];
-          temp = [40 55 65 73 78 84 90 95];
-        }
-      ];
-    };
-  };
-
-  firmwareCfg = firmwareSystem.config;
-  nvidiaCfg = nvidiaSystem.config;
-  fanCurveCfg = fanCurveSystem.config;
-  fanCurveText = fanCurveCfg.services.smartcool.asusdFanCurves.renderedText;
+  basicCfg = basicSystem.config;
 in {
-  module-firmware-attributes = pkgs.runCommand "smartcool-module-firmware-attributes" {} ''
-    test "${firmwareCfg.systemd.services.smartcool-firmware-attributes.serviceConfig.Type}" = oneshot
-    test -z "${toString (firmwareCfg.systemd.services.smartcool-firmware-attributes.serviceConfig.RemainAfterExit or "")}"
-    test "${firmwareCfg.systemd.timers.smartcool-firmware-attributes.timerConfig.Unit}" = smartcool-firmware-attributes.service
-    test "${firmwareCfg.systemd.timers.smartcool-firmware-attributes.timerConfig.OnUnitActiveSec}" = 60s
-    grep -F 'write_attr nv_temp_target 75' ${firmwareCfg.systemd.services.smartcool-firmware-attributes.serviceConfig.ExecStart}
-    grep -F 'outside firmware range' ${firmwareCfg.systemd.services.smartcool-firmware-attributes.serviceConfig.ExecStart}
-    touch $out
-  '';
-
-  module-nvidia-clock-cap = pkgs.runCommand "smartcool-module-nvidia-clock-cap" {} ''
-    test "${nvidiaCfg.systemd.services.smartcool-nvidia-clock-cap.serviceConfig.Type}" = oneshot
-    test "${toString nvidiaCfg.systemd.services.smartcool-nvidia-clock-cap.serviceConfig.RemainAfterExit}" = 1
-    test "${nvidiaCfg.systemd.timers.smartcool-nvidia-clock-cap.timerConfig.Unit}" = smartcool-nvidia-clock-cap-reassert.service
-    case ${nixpkgs.lib.escapeShellArg nvidiaCfg.systemd.services.smartcool-nvidia-clock-cap.serviceConfig.ExecStart} in
-      *"-lgc 210,600"*) ;;
+  module-daemon = pkgs.runCommand "smartcool-module-daemon" {} ''
+    test "${basicCfg.systemd.services.smartcool.serviceConfig.Type}" = notify
+    case ${nixpkgs.lib.escapeShellArg basicCfg.systemd.services.smartcool.serviceConfig.ExecStart} in
+      *"/bin/sc daemon -c /etc/smartcool/config.ron"*) ;;
       *) exit 1 ;;
     esac
-    case ${nixpkgs.lib.escapeShellArg nvidiaCfg.systemd.services.smartcool-nvidia-clock-cap.serviceConfig.ExecStop} in
-      *"-rgc"*) ;;
-      *) exit 1 ;;
-    esac
+    grep -F 'poll_interval_ms: 2000' ${basicCfg.environment.etc."smartcool/config.ron".source}
     touch $out
   '';
-
-  module-asusd-fan-curves = pkgs.writeText "smartcool-module-asusd-fan-curves" (
-    assert builtins.match ".*profiles: \\(.*quiet: \\[.*fan: CPU.*pwm: \\(0, 0, 0, 0, 30, 80, 140, 200\\).*temp: \\(40, 55, 65, 73, 78, 84, 90, 95\\).*" fanCurveText != null;
-    assert fanCurveCfg.environment.etc."asusd/fan_curves.ron".text == fanCurveText;
-    "ok\n"
-  );
 }
