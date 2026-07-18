8c5b84bbbe5d95787901ab8b7523d7ccf61c4e60
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-07-18 07:25:40 +0200

    chore: refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
 
     rs-harbor = {
-      url = "git+ssh://git@codeberg.org/caniko/rs-harbor.git?ref=trunk";
+      url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=9bfa8bdb0ecb22d7bc11448665f7fbaebae7a759";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.crane.follows = "crane";
     };
