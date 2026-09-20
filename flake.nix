f014b8c61747c30bfc6c65bfda81cfeddee83856
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-20 16:21:30 +0200

    chore(nix): bump harbor-rs pin to ed89d0b
    Pick up the fail-closed sccache wrapper with the private ephemeral
    fallback for transport-less hosts. Verified with nix flake check
    --no-build (all checks passed).

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
 
     harbor-rs = {
-      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=fac8049316846e0ef1c1e6acd92aed7a337b333a";
+      url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=ed89d0b13fc61dd1b2217bf4bba97f32cec27ba7";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.crane.follows = "crane";
       inputs.rust-overlay.follows = "rust-overlay";
